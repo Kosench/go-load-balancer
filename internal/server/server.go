2f92eaa8c0b55ffbee5b3044893856af32553190
Kosench <dimako36@mail.ru>
2025-05-11 05:35:33 +1000

    feat: implement HTTP server with load balancing and logging

---
+++ internal/server/server.go
@@ -0,0 +1,40 @@
+package server
+
+import (
+	"load-balancer/internal/balancer"
+	"load-balancer/internal/config"
+	"load-balancer/pkg/utils"
+	"net/http"
+)
+
+type Server struct {
+	Config   *config.Config
+	Balancer *balancer.Balancer
+	Logger   *utils.Logger
+}
+
+func NewServer(cfg *config.Config) *Server {
+	logger := utils.NewLogger(cfg.LogLevel)
+	return &Server{
+		Config:   cfg,
+		Balancer: balancer.NewBalancer(cfg.Backends),
+		Logger:   logger,
+	}
+}
+
+func (s *Server) Start() error {
+	http.HandleFunc("/", s.handleRequest)
+	s.Logger.Info("Starting server on %s", s.Config.ListenAddress)
+	return http.ListenAndServe(s.Config.ListenAddress, nil)
+}
+
+func (s *Server) handleRequest(w http.ResponseWriter, r *http.Request) {
+	s.Logger.Debug("Incoming request: %s %s", r.Method, r.URL.Path)
+
+	upstream := s.Balancer.GetNext()
+	if upstream == "" {
+		s.Logger.Error("No available backends")
+		http.Error(w, "No available backends", http.StatusServiceUnavailable)
+		return
+	}
+}
