ae2b2e1fd17c4ff57da71f82c3e0a9d36fe91057
Kosench <dimako36@mail.ru>
2025-05-20 19:25:55 +1000

    refactor(backend): use Backend struct and add startup synchronization in StartBackend

--- internal/backend/backend.go
+++ internal/backend/backend.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/rs/zerolog/log"
+	"sync"
 	"sync/atomic"
 	"time"
 
@@ -28,45 +29,50 @@ func (b *Backend) SetAlive(state bool) {
 }
 
 func StartBackend(ctx context.Context, backends []*Backend) {
-	for _, backend := range backends {
-		go func(addr string) {
+	var wg sync.WaitGroup
+	for _, b := range backends {
+		wg.Add(1)
+
+		go func(b *Backend) {
+			defer wg.Done()
+
 			mux := http.NewServeMux()
 			mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-				fmt.Fprintf(w, "Response from backend %s", addr)
+				fmt.Fprintf(w, "Response from backend %s", b.Addr)
 			})
 			mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 				w.WriteHeader(http.StatusOK)
 			})
 
 			srv := &http.Server{
-				Addr:    addr,
+				Addr:    b.Addr,
 				Handler: mux,
 			}
 
 			go func() {
-				log.Info().Str("addres", addr).Msg("Starting backend server")
+				log.Info().Str("addres", b.Addr).Msg("Starting backend server")
 				if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 					log.Error().
 						Err(err).
-						Str("address", addr).
+						Str("address", b.Addr).
 						Msg("Failed to start backend server")
 				}
 			}()
 
 			<-ctx.Done()
-			log.Info().Str("address", addr).Msg("Shutting down backend server")
+			log.Info().Str("address", b.Addr).Msg("Shutting down backend server")
 
 			shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 			defer cancel()
 			if err := srv.Shutdown(shutdownCtx); err != nil {
 				log.Error().
 					Err(err).
-					Str("address", addr).
+					Str("address", b.Addr).
 					Msg("Backend server shutdown failed")
 			}
-			log.Info().Str("address", addr).Msg("Backend server stopped")
-		}(backend.Addr)
+			log.Info().Str("address", b.Addr).Msg("Backend server stopped")
+		}(b)
 	}
 
-	return
+	wg.Wait()
 }
