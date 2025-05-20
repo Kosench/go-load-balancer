1f80e5e5e049800b63ce0f1af97b2f654685db08
Kosench <dimako36@mail.ru>
2025-05-20 19:13:13 +1000

    update cfg

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,12 +2,11 @@ package config
 
 import (
 	"github.com/spf13/viper"
-	"strings"
 )
 
 type Config struct {
-	ListenAddress string
-	Backends      []string
+	ListenAddress string   `mapstructure:"ListenAddress"`
+	Backends      []string `mapstructure:"Backends"`
 }
 
 func LoadConfig(path string) (*Config, error) {
@@ -20,16 +19,14 @@ func LoadConfig(path string) (*Config, error) {
 	viper.SetDefault("ListenAddress", ":8080")
 	viper.SetDefault("BACKENDS", []string{"localhost:9001", "localhost:9002"})
 
-	viper.BindEnv("BACKENDS")
-
 	if err := viper.ReadInConfig(); err != nil {
 		return nil, err
 	}
 
-	cfg := &Config{
-		ListenAddress: viper.GetString("ListenAddress"),
-		Backends:      strings.Split(viper.GetString("BACKENDS"), ","),
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, err
 	}
 
-	return cfg, nil
+	return &cfg, nil
 }
